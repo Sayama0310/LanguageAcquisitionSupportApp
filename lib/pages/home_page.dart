384e1652c96dbfe02f190edadaf80b7e00d4b64d
Sayama0310 <4696monochrome@ezweb.ne.jp>
2022-12-01 00:01:23 +0900

    [feat] 問題文などの表示をざっくりと作成

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,7 +17,71 @@ class HomePage extends StatelessWidget {
             style: TextStyle(color: Colors.white),
           ),
         ),
-        body: const Text("Hello World"),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Hello",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "こんにちは",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Ink(
+                    decoration: const ShapeDecoration(
+                      color: Colors.amber,
+                      shape: CircleBorder(),
+                    ),
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_left),
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Ink(
+                    decoration: const ShapeDecoration(
+                      color: Colors.amberAccent,
+                      shape: CircleBorder(),
+                    ),
+                    child: IconButton(
+                      icon: const Icon(Icons.check),
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Ink(
+                    decoration: const ShapeDecoration(
+                      color: Colors.amber,
+                      shape: CircleBorder(),
+                    ),
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_right),
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
